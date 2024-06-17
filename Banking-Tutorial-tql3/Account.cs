0184a85f3608f4e4dc72cfa7ea03765e0e795908
Greg Doud <gpdoud@gmail.com>
2024-06-17 14:14:21 -0400

    Add project files.

---
+++ Banking-Tutorial-tql3/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Tutorial_tql3;
+
+public class Account {
+
+    public int AccountId { get; set; } = 0;
+    public string Description { get; set; } = string.Empty;
+    public decimal Balance { get; set; } = 0;
+
+    public bool Deposit(decimal Amount) {
+        if(Amount <= 0) {
+            Console.WriteLine("Amount cannot be zero or negative!");
+            return false;
+        }
+        Balance += Amount;
+        return true;
+    }
+
+    public bool Withdraw(decimal Amount) {
+        if(Amount <= 0) {
+            Console.WriteLine("Amount cannot be zero or negative!");
+            return false;
+        }
+        if(Amount > Balance) {
+            Console.WriteLine("Insufficient Funds!");
+            return false;
+        }
+        Balance -= Amount;
+        return true;
+    }
+
+    public bool Transfer(decimal Amount, Account ax) {
+        var success = this.Withdraw(Amount);
+        if(success == true) {
+            ax.Deposit(Amount);
+        }
+        return true;
+    }
+}
---
+++ Banking-Tutorial-tql3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Banking_Tutorial_tql3;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        Account a1 = new Account();
+        Account a2 = new Account();
+
+        Console.WriteLine($"Balance is {a1.Balance:C}");
+        a1.Deposit(500);
+        Console.WriteLine($"Balance is {a1.Balance:C}");
+        a1.Withdraw(230);
+        Console.WriteLine($"Balance is {a1.Balance:C}");
+        a1.Withdraw(300);
+        Console.WriteLine($"Balance is {a1.Balance:C}");
+        a1.Deposit(-200);
+        Console.WriteLine($"Balance is {a1.Balance:C}");
+
+        a1.Transfer(100, a2);
+
+        a2.Transfer(50, a1);
+    }
+}
